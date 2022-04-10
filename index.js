8ba73fae60576494bc96688e0c79d6553eba71f7
Serje <Serje.havandjian@gmail.com>
2022-04-10 10:52:03 -0400

    commit tree 9e2a0d8a1af8121c7908d8ed290c216c09fe8fd1

--- index.js
+++ index.js
@@ -25,24 +25,70 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ")
+
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+
+const [blackAndWhite, , black, pink, ] = farmAnimals.split(" ")
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
 
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
+// const [r, o, y, g, b, ,v] = colors
+
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+
+const [r, o, y, g, b, indg, v] = colors
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
+const muppetName = muppet.muppetName
+const color = muppet.color
+const song = muppet.song
+const job = muppet.job
+const partner = muppet.partner
+
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const song2  = nestedMuppet.album.theMuppetMovie.song2
+const song4 = nestedMuppet.album.theMuppetMovie.song4
+
+const nestedJob = nestedMuppet.nestedJob
+const nestedPartner = nestedMuppet.nestedPartner
+
+// const {song4} = nestedMuppet.theMuppetMovie.song4
+
+
+// const {nestedJob} = nestedMuppet.nestedJob
+// const {nestedPartner} = nestedMuppet.nestedPartner
+
+// const nestedMuppet = {
+//   nestedName: 'Kermit',
+//   nestedColor: 'green',
+//   album: {
+//     theMuppetMovie: {
+//       song1: 'Rainbow Connection',
+//       song2: 'Moving Right Along',
+//       song3: 'Never Before, Never Again',
+//       song4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   nestedJob: 'Host of The Muppet Show',
+//   nestedPartner: 'Miss Piggy'
+// };
